7afbbb898c06a4007140dd68c1b36139e4e6baff
Kulkarni <amruta-milind.kulkarni@capgemini.com>
2022-12-30 12:38:10 +0530

    Assignment 4 based on custom exception handling

---
+++ c#/OOP_Assignment_4-Custom_Exception/OOP_Assignment_4-Custom_Exception/MyStack.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_4_Custom_Exception_
+{
+    public class StackException : Exception
+    {
+        public StackException(string message) : base(message) { }
+    }
+    public class MyStack : ICloneable
+    {
+        private int[] arr = new int[20];
+        private int top = -1;
+        private int size;
+
+        public object Clone()
+        {
+            Console.WriteLine("Cloned Array");
+            MyStack newStack = new MyStack
+            {
+                arr = this.arr,
+                top = this.top,
+                size = this.size
+            };
+            return newStack;
+        }
+        public int SIZE { get { return size; } set { this.size = arr.Length; } }
+
+        public void push(int a)
+        {
+            try
+            {
+                if (top == arr.Length - 1)
+                {
+                    throw new StackException("Stack full Exception");
+                }
+                arr[++top] = a;
+                Console.WriteLine($"Pushed {a} onto stack");
+            }
+            catch (StackException st)
+            {
+                Console.WriteLine(st);
+            }
+        }
+        public void POP()
+        {
+            try
+            {
+                if (top == -1)
+                {
+                    throw new StackException("Stack Empty Exception");
+
+                }
+                Console.WriteLine($"{arr[top]} popped");
+                arr[top--] = 0;
+            }
+            catch (StackException st)
+            {
+                Console.WriteLine(st);
+            }
+        }
+        public void display()
+        {
+            Console.WriteLine("Printing Array:");
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
---
+++ c#/OOP_Assignment_4-Custom_Exception/OOP_Assignment_4-Custom_Exception/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Assignment_4_Custom_Exception_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ch;
+            MyStack s = new MyStack();
+            do
+            {
+                Console.WriteLine("Enter choice");
+                Console.WriteLine("1.Push 2.POP 3.Display 4.Clone 5.Exit");
+                ch = Convert.ToInt32(Console.ReadLine());
+
+                switch (ch)
+                {
+                    case 1:
+                        Console.WriteLine("Enter elements");
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        s.push(n);
+                        break;
+                    case 2:
+                        s.POP();
+                        break;
+                    case 3:
+                        s.display();
+                        break;
+                    case 4:
+                        MyStack newStack = s.Clone() as MyStack;
+                        newStack.display();
+                        break;
+                    case 5: break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
+            } while (ch < 5);
+        }
+    }
+}
