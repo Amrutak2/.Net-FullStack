85c0dce6b611124bbe1653f6566e1c86e4485f74
Kulkarni <amruta-milind.kulkarni@capgemini.com>
2022-12-30 12:40:10 +0530

    Assignment 5-3 based on Collection Framework

---
+++ c#/OOP_Assignment-5-3/OOP_Assignment-5-3/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Assignment_5_3
+{
+    class Employee
+    {
+        public string name;
+        public Employee(string name)
+        {
+            this.name = name;
+        }
+        public override string ToString()
+        {
+            return (name);
+        }
+    }
+}
---
+++ c#/OOP_Assignment-5-3/OOP_Assignment-5-3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace OOP_Assignment_5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> li = new List<Employee>();
+
+            Console.WriteLine("Enter the number of Employees you want:");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the names of employee :");
+            for (int i = 0; i < size; i++)
+            {
+                string name = Console.ReadLine();
+                li.Add(new Employee(name));
+            }
+
+            Console.WriteLine("The list of employee is :");
+            foreach (Employee emp in li)
+            {
+                Console.WriteLine(emp);
+            }
+            Console.WriteLine("List Contains " + li.Count + " Employees");
+        }
+    }
+}
