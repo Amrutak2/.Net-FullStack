75e56e3f87fdb0b79da0617cd25cff86f93d2799
Kulkarni <amruta-milind.kulkarni@capgemini.com>
2022-12-30 12:37:01 +0530

    OOP Assignment 3

---
+++ c#/OOP_Assignment_3/OOP_Assignment_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP_Assignment_3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Manager emp = new Manager();
+            MarketingExecutive M = new MarketingExecutive();
+            Console.WriteLine("Enter Employee Number: ");
+            emp.EMPNO = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employee name: ");
+            emp.EMPNAME = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter Employee Salary: ");
+            emp.SALARY = Convert.ToDouble(Console.ReadLine());
+            //emp.CalculateDetails();
+            //emp.DisplayEmpDetails();
+
+            emp.CalculateSalary();
+            emp.CalculateDetails();
+            emp.DisplayManDetails();
+            M.CalculteMarketing();
+            M.CalculateDetails();
+            M.DisplayMarketing();
+            Console.ReadKey();
+        }
+    }
+}
+
