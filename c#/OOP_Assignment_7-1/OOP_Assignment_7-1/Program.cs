395f8f5569c51dc81a10c5a88c844906da29739d
Kulkarni <amruta-milind.kulkarni@capgemini.com>
2023-01-02 10:57:19 +0530

    Assignment 7 based on File IO

---
+++ c#/OOP_Assignment_7-1/OOP_Assignment_7-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace OOP_Assignment_7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Github\.Net-FullStack\c#\OOP_Assignment_7-1\OOP_Assignment_7-1";
+
+            var dirs = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+            var files = Directory.GetFiles(rootPath, "*.txt", SearchOption.AllDirectories);
+
+            Console.WriteLine("***************Directories***************");
+            Console.Write("\n");
+            foreach (string dir in dirs)
+            {
+                var dirInfo = new DirectoryInfo(dir);
+                Console.WriteLine($"{dirInfo.FullName}");
+            }
+            Console.WriteLine("\n***************All .txt files***************");
+            Console.Write("\n");
+            foreach (string file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                Console.WriteLine($"{Path.GetFileName(file)} - { fileInfo.Length } bytes");
+            }
+        }
+    }
+}
