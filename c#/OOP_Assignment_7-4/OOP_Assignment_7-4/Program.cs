50cad0af26e80d39a6dd248642c0236b15aa2b2b
Kulkarni <amruta-milind.kulkarni@capgemini.com>
2023-01-06 16:54:39 +0530

    OOP_Assignment 7-4

---
+++ c#/OOP_Assignment_7-4/OOP_Assignment_7-4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace OOP_Assignment_7_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"C:\Github\.Net-FullStack\c#\OOP_Assignment_7-4\Output.txt";
+            Employee emp = new Employee(301, "Amruta", 56000);
+            FileStream stream = new FileStream(file, FileMode.OpenOrCreate);
+            BinaryFormatter bf = new BinaryFormatter();
+
+
+            bf.Serialize(stream, emp);
+            stream.Close();
+            Console.WriteLine(" File created successfully -> " + file);
+
+
+            FileStream stream1 = new FileStream(file, FileMode.Open, FileAccess.Read);
+            Employee objnew = (Employee)bf.Deserialize(stream1);
+
+            Console.WriteLine(objnew.id);
+            Console.WriteLine(objnew.name);
+            Console.WriteLine(objnew.salary);
+        }
+    }
+}
