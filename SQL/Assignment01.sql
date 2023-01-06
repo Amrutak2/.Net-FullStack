16ed9553baccef371aa492104f29a08624d74ed2
Kulkarni <amruta-milind.kulkarni@capgemini.com>
2023-01-06 10:20:29 +0530

    SQL Assignment-1,2,3

--- SQL/Assignment01.sql
+++ SQL/Assignment01.sql
@@ -3,6 +3,8 @@ insert into Customer values(1, 'Amruta', 'Kulkarni', 'Dhule', 'India', '88779911
 insert into Customer values(2, 'Bhakti', 'Takane', 'Amalner', 'India', '6677991133' );
 insert into Customer values(3, 'Srishti', 'Patil', 'Dallas', 'America', '8877990092' );
 insert into Customer values(4, 'Raksha', 'Gujrathi', 'seoul', 'Korea', '8876591122' );
+insert into Customer values(5, 'Shiny', 'Majumdar', 'Sydney', 'Australia', '030-0074321' );
+insert into Customer values(6, 'Shirley', 'William', 'London', 'UK', '7722991123');
 
 Select * from Customer
 
@@ -12,3 +14,69 @@ order by Country
 
 select FirstName from Customer
 where FirstName like '__i%'
+
+
+insert into Orders values(100, '2020/10/23 10:21:43:121', 5, 1, 749.00 )
+insert into Orders values(101, '2021/10/28 05:34:55:222', 2, 5, 599.00 )
+insert into Orders values(102, '2022/05/02 02:52:11:452', 4, 2, 379.00 )
+insert into Orders values(103, '2023/01/05 22:13:44:233', 6, 1, 800.00 )
+insert into Orders values(104, '2020/12/09 12:33:56:123', 3, 3, 237.00 )
+select * from Orders
+
+delete from Orders where TotalAmount = 699.00;
+
+update Orders set OrderDate = '2020/10/23 10:21:43:121' where Id = '100';
+update Orders set OrderDate = '2021/10/28 05:34:55:222' where Id = '101';
+update Orders set OrderDate = '2022/05/02 02:52:11:452' where Id = '102';
+update Orders set OrderDate = '2023/01/05 22:13:44:233' where Id = '103';
+update Orders set OrderDate = '2020/12/09 12:33:56:123' where Id = '104';
+
+insert into Product values(200, 'Chips', 50.00, 'Lays', 0);
+insert into Product values(201, 'Chai', 30.00, 'Taj', 1);
+insert into Product values(203, 'Coffee', 40.00, 'Bru', 0);
+insert into Product values(202, 'Biscuits', 60.00, 'Burborn', 0);
+insert into Product values(204, 'Popcorn', 20.00, 'Caramel', 1);
+insert into Product values(205, 'chai', 50.00, 'Taj', 1, 'Refreshing Drink');
+delete from Product where Id = 205;
+
+select * from Product
+
+insert into OrderItem values(10, 100, 200, 649.00, 3);
+insert into OrderItem values(12, 103, 201, 449.00, 5);
+insert into OrderItem values(13, 102, 202, 549.00, 3);
+insert into OrderItem values(14, 101, 203, 749.00, 2);
+insert into OrderItem values(15, 104, 204, 349.00, 6);
+insert into OrderItem values(16, 103, 205, 50.00, 6);
+delete from OrderItem where ProductId = 205;
+
+select * from OrderItem;
+
+select * from Orders where CustomerId = (select Id from Customer where Phone = '030-0074321');
+
+alter table Product add Category nvarchar(40);
+update Product set Category = 'Refreshing Drink' where Id = '201';
+update Product set Category = 'Snacks' where Id = '200';
+update Product set Category = 'Snacks' where Id = '202';
+update Product set Category = 'Seafood', ProductName = 'Fish' where Id = '204';
+update Product set Category = 'Refreshing Drink' where Id = '203';
+update Product set Package = 'Lays' where Id = '200';
+
+select * from Product where Category = 'Seafood';
+Select * from Orders inner join Customer on Orders.CustomerId = Customer.id where Customer.City!='London';
+select * from OrderItem where ProductId = (Select Id from Product where ProductName ='Chai');
+
+Create table Employee(
+FirstName nvarchar(40),
+LastName nvarchar(40),
+Department nvarchar(40),
+Manager nvarchar(40),
+Rating int,
+Salary int);
+
+insert into Employee values('Amruta', 'Kulkarni','CSE', 'Rashmi', 5, 25000);
+insert into Employee values('Apurva', 'Gondkar','Civil', 'Rashmi', 6, 2300);
+insert into Employee values('Rutuja', 'Thombre','Civil', 'Rashmi', 5, 26000);
+insert into Employee values('Nikita', 'Punde','Entc', 'Rashmi', 5, 35000);
+insert into Employee values('Gauri', 'Devahdrao','Civil', 'Rashmi', 6, 22000);
+
+select * from Employee;
