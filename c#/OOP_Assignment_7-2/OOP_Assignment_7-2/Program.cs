098ea08ba5a7c07e6c56eae0aef85b7e544415ce
Kulkarni <amruta-milind.kulkarni@capgemini.com>
2023-01-06 16:46:48 +0530

    OOP_Assignment 7-2

---
+++ c#/OOP_Assignment_7-2/OOP_Assignment_7-2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+
+namespace OOP_Assignment_7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"C:\Github\.Net-FullStack\c#\OOP_Assignment_7-2\Output.txt";
+            Console.WriteLine("Customer Details");
+            Console.WriteLine("Enter Account Number: ");
+            int Acc_Number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Customer Name: ");
+            string Customer_Name = Console.ReadLine();
+            Console.WriteLine("Enter Balance: ");
+            int Balance = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            using (StreamWriter writer = new StreamWriter(file))
+            {
+                writer.Write("Customer Name:" + Customer_Name + "\nAccount Number:" + Acc_Number + "\nBalance:" + Balance);
+            }
+            Console.WriteLine("Saved");
+
+
+
+            Console.WriteLine("\n\n*******Reading Text File*******");
+
+
+
+            using (StreamReader reader = new StreamReader(file))
+            {
+                Console.WriteLine(reader.ReadToEnd());
+            }
+        }
+    }
+}
