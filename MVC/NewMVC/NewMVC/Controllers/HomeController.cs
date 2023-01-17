3de8cdded81e1b9294eec2a57f25520e903891f6
Kulkarni <amruta-milind.kulkarni@capgemini.com>
2023-01-17 09:57:46 +0530

    MVC demo

---
+++ MVC/NewMVC/NewMVC/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NewMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+
+        //public string Name()
+        //{
+        //    return "Amruta";
+        //}
+
+        //public string Details()
+        //{
+        //    return "Details";
+        //}
+        //public string Index(string id, string name)
+        //{
+        //    return "Hello, "+ name + "id = "+id;
+        //}
+        public ActionResult Index()
+        {
+            ViewBag.Countries = new List<string>()
+            {
+                "India",
+                "US",
+                "UK",
+                "Canada"
+
+            };
+            return View();
+        }
+
+    }
+}
---
+++ MVC/NewMVC/NewMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NewMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
